dd5a1339cbbdc0c83e83f87f2d32945dc338591f
life-f <58433392+life-f@users.noreply.github.com>
2020-04-15 10:45:37 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file main.cpp
+ * @author Bagrova Polina
+ */
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
